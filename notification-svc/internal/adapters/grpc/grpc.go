022fbd539d1311284486b341f3412f80e35d9fa7
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-08-17 17:44:36 +0300

    add device cache logic

--- notification-svc/internal/adapters/grpc/grpc.go
+++ notification-svc/internal/adapters/grpc/grpc.go
@@ -7,13 +7,23 @@ import (
 	"github.com/nico-phil/notification/internal/application/core/domain"
 )
 
+var deviceCahe map[int64]domain.Device = make(map[int64]domain.Device)
+
 func(a Adapter) Push(ctx context.Context, request *notif.SendPushNotificationsRequest)(*notif.SendPushNotificationsResponse, error){
 
-	device, err := a.api.GetDevice(ctx, 1)
-	
+	var device domain.Device
+	var err error
+	device, ok := deviceCahe[1]
+	if !ok {
+		device, err = a.api.GetDevice(ctx, 1)
+	}
+
 	if err != nil {
 		return &notif.SendPushNotificationsResponse{Sent: false}, err
 	}
+	
+	deviceCahe[int64(device.ID)] = device
+	
 	pushNotification := domain.NewPushNotification(request.Content, request.Title,  device)
 	
 	a.api.SendPushNotification(ctx, pushNotification)
