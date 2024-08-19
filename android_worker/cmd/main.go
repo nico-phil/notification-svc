1bd23b96c4b6bb4e8e398cd266eff0117eb867b5
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-08-19 07:59:32 +0300

    add validation error to push notif request

--- android_worker/cmd/main.go
+++ android_worker/cmd/main.go
@@ -9,7 +9,6 @@ import (
 
 func main(){
 
-	
 	fcmAdapter := &fcm.Adapter{}
 
 	err := fcmAdapter.GenerateToken()
--- notification-svc/internal/adapters/grpc/grpc.go
+++ notification-svc/internal/adapters/grpc/grpc.go
@@ -5,11 +5,44 @@ import (
 
 	notif "github.com/nico-phil/notification-proto/golang/notification"
 	"github.com/nico-phil/notification/internal/application/core/domain"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var deviceCahe map[int64]domain.Device = make(map[int64]domain.Device)
 
 func(a Adapter) Push(ctx context.Context, request *notif.SendPushNotificationsRequest)(*notif.SendPushNotificationsResponse, error){
+
+	var validationErrors []*errdetails.BadRequest_FieldViolation
+	if len(request.Title) == 0 {
+		validationErrors = append(validationErrors, &errdetails.BadRequest_FieldViolation{
+			Field: "title",
+			Description: "title cannot be empty",
+		})
+	}
+
+	if len(request.Content) == 0 {
+		validationErrors = append(validationErrors, &errdetails.BadRequest_FieldViolation{
+			Field: "content",
+			Description: "content cannot be empty",
+		})
+	}
+
+	if len(request.DeviceId) == 0 {
+		validationErrors = append(validationErrors, &errdetails.BadRequest_FieldViolation{
+			Field: "title",
+			Description: "device id cannot be negative",
+		})
+	}
+
+	if len(validationErrors) > 0 {
+		stat := status.New(codes.InvalidArgument , "invalid push notification request" )
+		badRequest := &errdetails.BadRequest{}
+		badRequest.FieldViolations = validationErrors
+		s, _ := stat.WithDetails(badRequest)
+		return nil, s.Err()
+	}
 
 	var device domain.Device
 	var err error
