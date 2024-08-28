02811a0876398d8afed12423a1209ea1d345e900
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-08-28 18:39:02 +0300

    add unit test for user-svc api

--- user-svc/go.mod
+++ user-svc/go.mod
@@ -3,13 +3,21 @@ module github.com/nico-phil/notification/user-svc
 go 1.22.2
 
 require (
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/nico-phil/notification-proto/golang/user v1.0.6 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
+	github.com/lib/pq v1.10.9
+	github.com/nico-phil/notification-proto/golang/user v1.0.6
+	github.com/stretchr/testify v1.9.0
+	golang.org/x/crypto v0.26.0
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240826202546-f6391c0de4c7
+	google.golang.org/grpc v1.65.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240826202546-f6391c0de4c7 // indirect
-	google.golang.org/grpc v1.65.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ user-svc/internal/application/core/api/api_test.go
@@ -0,0 +1,92 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/nico-phil/notification/user-svc/internal/application/core/domain"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type mockedDB struct {
+	mock.Mock
+}
+
+func(d *mockedDB) SaveUser(ctx context.Context, user *domain.User) error{
+	args := d.Called(ctx, user)
+	return args.Error(0)
+}
+
+func(d *mockedDB) GetUser(ctx context.Context, id int64) (domain.User, error){
+	args := d.Called(ctx, id)
+	return args.Get(0).(domain.User), args.Error(1)
+}
+
+func(d *mockedDB) SaveDevice(ctx context.Context, device *domain.Device) error {
+	args := d.Called(ctx, device)
+	return args.Error(0)
+}
+
+func(d * mockedDB) GetUserDevice(ctx context.Context, userId int64)(domain.Device, error){
+	args := d.Called(ctx, userId)
+	return args.Get(0).(domain.Device), args.Error(1)
+}
+
+func Test_Create(t * testing.T){
+	db := new(mockedDB)
+	db.On("SaveUser", mock.Anything, mock.Anything).Return(nil)
+
+	u := domain.User{
+		Firstname: "jhon",
+		Lastname: "doe",
+		Email: "jhondoe@gmai.com",
+		Password: "123456",
+	}
+	u.EncriptPassword()
+	application := NewApplication(db)
+	err := application.Create(context.Background(), &u)
+
+	assert.Nil(t,err)
+
+}
+
+func Test_Create_Should_Retur_Error_when_db_Persistent_Fail(t *testing.T){
+	db := new(mockedDB)
+	db.On("SaveUser", mock.Anything, mock.Anything).Return(errors.New("failed to save data"))
+
+	u := domain.User{
+		Firstname: "jhon",
+		Lastname: "doe",
+		Email: "jhondoe@gmai.com",
+		Password: "123456",
+	}
+	u.EncriptPassword()
+	application := NewApplication(db)
+	err := application.Create(context.Background(), &u)
+
+	assert.EqualError(t, err, "failed to save data")
+}
+
+func Test_Get(t *testing.T){
+	db := new(mockedDB)
+	db.On("GetUser", mock.Anything, mock.Anything).Return(domain.User{}, nil)
+
+	application := NewApplication(db)
+	u, err := application.db.GetUser(context.Background(), 1)
+
+	assert.Nil(t, err)
+	assert.Equal(t, u, domain.User{})
+}
+
+func Test_Get_Should_Return_Error_When_User_Not_Found(t *testing.T) {
+	db := new(mockedDB)
+	db.On("GetUser", mock.Anything, mock.Anything).Return(domain.User{}, errors.New("user not found"))
+
+	application := NewApplication(db)
+	_, err := application.db.GetUser(context.Background(), 1)
+
+	assert.EqualError(t, err, "user not found")
+}
+
